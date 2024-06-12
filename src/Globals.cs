89fca19250e2bb87a67c00b5e7e5f3bb98ca32f4
oscar <29248751+oscar-wos@users.noreply.github.com>
2024-06-13 00:57:01 +0100

    fix: Timer unloaded onMapChange

--- src/Globals.cs
+++ src/Globals.cs
@@ -7,13 +7,13 @@ namespace Zones;
 public partial class Zones
 {
     public override string ModuleName => "Retakes-Zones";
-    public override string ModuleVersion => "1.0.0";
+    public override string ModuleVersion => "1.0.1";
     public override string ModuleAuthor => "https://github.com/oscar-wos/Retakes-Zones";
     private static PluginCapability<IRetakesPluginEventSender> RetakesPluginEventSenderCapability { get; } = new("retakes_plugin:event_sender");
 
     private readonly List<Zone> _zones = [];
     private readonly Dictionary<int, List<Zone>> _playerZones = [];
+    private readonly Dictionary<int, List<Zone>> _playerGreenZones = [];
     private readonly Dictionary<int, Vector> _playerLastPosition = [];
     private readonly Dictionary<int, Vector> _playerLastVelocity = [];
-    private readonly Dictionary<int, List<Zone>> _playerGreenZones = [];
 }

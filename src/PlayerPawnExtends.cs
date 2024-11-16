0e642f134bf0462a510e385b6d801f0ba600c731
Oscar <29248751+oscar-wos@users.noreply.github.com>
2024-11-16 01:55:03 +0000

    refactor: pawn -> playerPawn

--- src/PlayerPawnExtends.cs
+++ src/PlayerPawnExtends.cs
@@ -5,13 +5,13 @@ namespace Zones;
 
 public static class PlayerPawnExtends
 {
-    public static void Bounce(this CCSPlayerPawn pawn)
+    public static void Bounce(this CCSPlayerPawn playerPawn)
     {
-        var vel = new Vector(pawn.AbsVelocity.X, pawn.AbsVelocity.Y, pawn.AbsVelocity.Z);
+        var vel = new Vector(playerPawn.AbsVelocity.X, playerPawn.AbsVelocity.Y, playerPawn.AbsVelocity.Z);
         var speed = Math.Sqrt(vel.X * vel.X + vel.Y * vel.Y);
 
         vel *= (-350 / (float)speed);
         vel.Z = vel.Z <= 0 ? 150 : Math.Min(vel.Z, 150);
-        pawn.Teleport(pawn.AbsOrigin, pawn.EyeAngles, vel);
+        playerPawn.Teleport(playerPawn.AbsOrigin, playerPawn.EyeAngles, vel);
     }
 }
